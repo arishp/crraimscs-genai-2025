560801ce588e5dea2ef1722cfe254d8710d8b92c
arish-pitchai_pwcit <arish.pitchai@pwc.com>
2025-01-28 17:14:15 +0530

    crime RAG completed

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
 import streamlit as st
 
-try:
-    file_uploaded = st.file_uploader("Upload file")
-except:
-    st.write('Unable to create vector db')
-
 user_query = st.text_input("Enter your query")
 
 if len(user_query.strip()) > 0:
